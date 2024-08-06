d98d8f1a5b4adaebfdd8d946737c1331b75b54a5
samuel bazaga <samuelbazaga@gmail.com>
2024-08-07 00:31:47 +0200

    feat: Add support for fragile items and refactor code

--- TypeScript/app/facer-store.ts
+++ TypeScript/app/facer-store.ts
@@ -26,54 +26,129 @@ export class FacerStore {
   constructor(items = [] as Array<Item>) {
     this.items = items;
   }
-
+  
+  /**
+   * Updates the quality of all items in the store by iterating over them
+   * and calling the appropriate update method based on the item type
+   */
   updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Vintage Rolex' && this.items[i].name != 'Passes to Watchface Conference') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Legendary Watch Face') {
-            this.items[i].quality = this.items[i].quality - 1
-          }
-        }
+    this.items = this.items.map((item) => {
+      if (item.name === 'Vintage Rolex') {
+        return this.updateVintageRolex(item);
+      } else if (item.name === 'Passes to Watchface Conference') {
+        return this.updateConferenceItem(item);
+      } else if (item.name === 'Legendary Watch Face') {
+        return this.updateLegendaryItem(item);
+      } else if (item instanceof FragileItem) {
+        return this.updateFragileItem(item);
       } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1
-          if (this.items[i].name == 'Passes to Watchface Conference') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-          }
-        }
+        // If the item is not a special item, update it based on the general rules
+        // These might be unknown items, so more specific rules can be added as needed
+        return this.updateRegularItem(item);
       }
-      if (this.items[i].name != 'Legendary Watch Face') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Vintage Rolex') {
-          if (this.items[i].name != 'Passes to Watchface Conference') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Legendary Watch Face') {
-                this.items[i].quality = this.items[i].quality - 1
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1
-          }
-        }
+    });
+  }
+  /**
+   * Updates a regular item based on the general rules
+   * - Quality decreases by 1 each day
+   * - Quality decreases by 2 after sellIn date
+   * @param item Item to be updated
+   * @returns Updated item
+   */
+  updateRegularItem(item: Item) {
+    if (item.quality > 0) {
+      item = this.decreaseQuality(item, 1);
+    }
+    item.sellIn -= 1;
+    if (item.sellIn < 0) {
+      if (item.quality > 0) {
+        // Quality decreases twice as fast after sellIn date
+        // We decrease quality by 1 again to make it a total of 2
+        item = this.decreaseQuality(item, 1);
       }
     }
+    return item;
+  }
 
-    return this.items;
+  /**
+   * Updates a fragile item that degrade twice as fast as regular items
+   */
+  updateFragileItem(item: FragileItem) {
+    if (item.fragile) {
+      if (item.quality > 0) {
+        item = this.decreaseQuality(item, 2) as FragileItem;
+      }
+      item.sellIn -= 1;
+      if (item.sellIn < 0) {
+        if (item.quality > 0) {
+          item = this.decreaseQuality(item, 2) as FragileItem;
+        }
+      }
+    } else {
+      throw new Error('Invalid item type');
+    }
+    return item;
+  }
+
+  /**
+   * Updates a Vintage Rolex item that increases in quality over time
+   */
+  updateVintageRolex(item: Item) {
+    item = this.increaseQuality(item, 1);
+    return item;
+  }
+
+  /**
+   * Legendary items never change
+   * This method is here so we can easily add more specific rules for legendary items in the future
+   */
+  updateLegendaryItem(item: Item) {
+    if (item.name !== 'Legendary Watch Face') {
+      throw new Error('Invalid item type');
+    }
+    // Legendary items never change
+    return item;
+  }
+
+  /**
+   * Update conference item based on conference rules
+   * - Quality increases by 1 each day
+   * - Quality increases by 2 when sellIn is 10 or less
+   * - Quality increases by 3 when sellIn is 5 or less
+   * - Quality drops to 0 after sellIn date
+   */
+  updateConferenceItem(item: Item) {
+    if (item.name === 'Passes to Watchface Conference') {
+      if (item.sellIn <= 0) {
+        item.quality = 0;
+      } else if (item.sellIn <= 5) {
+        item = this.increaseQuality(item, 3);
+      } else if (item.sellIn <= 10) {
+        item = this.increaseQuality(item, 2);
+      } else {
+        item = this.increaseQuality(item, 1);
+      }
+    } else {
+      throw new Error('Invalid item type');
+    }
+    item.sellIn -= 1;
+    return item;
+  }
+
+  increaseQuality(item: Item, amount: number) {
+    if (item.quality < 50) {
+      item.quality += amount;
+    }
+    if (item.quality > 50) {
+      item.quality = 50;
+    }
+    return item;
+  }
+
+  decreaseQuality(item: Item, amount: number) {
+    if (item.quality > 0) {
+      item.quality -= amount;
+    }
+    return item;
   }
 }
